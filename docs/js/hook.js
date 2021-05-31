a8caf1503eebc99d69131c684c9d40cddc0fe1be
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2021-05-31 12:55:11 +0200

    updated folders, deleted duplicates

---
+++ docs/js/hook.js
@@ -0,0 +1,16 @@
+export class Hook {
+    constructor() {
+        this.create();
+    }
+    create() {
+        this.outerDiv = document.createElement("outerDiv");
+        this.div = document.createElement("hook");
+        this.outerDiv.appendChild(this.div);
+        document.body.appendChild(this.outerDiv);
+    }
+    shootHook() {
+        if (this.letter = correctChar) {
+        }
+    }
+}
+//# sourceMappingURL=hook.js.map
