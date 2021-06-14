869b483793529bc2452bcd49a7e40a6fce3f950c
lolnoodle <issam42069@gmail.com>
2021-06-14 12:09:55 +0200

    max fixed game and character class

--- dev/character.ts
+++ dev/character.ts
@@ -1,5 +1,4 @@
 import { transform } from "../node_modules/typescript/lib/typescript"
-
 export class Character {
     x : number = 0 
     y : number = 0
@@ -8,41 +7,32 @@ export class Character {
     value : string = ''
     onBelt : boolean
     spawnElement : HTMLElement
- 
     constructor(value : string){
         this.value = value
         this.spawnElement = document.querySelector('charcontainer')!;
         this.create()
     }
-
-
     //not working
     update() : void {
-        if(logkey = "KeyW") {
-            console.log("we movin");
-            this.x -= this.speed;
-            this.div.style.transform = `translate(${this.x}px, ${this.y}px)`
-        }
-       
+        // if(logkey = "KeyW") {
+        //     console.log("we movin");
+        //     this.x -= this.speed;
+        //     this.div.style.transform = `translate(${this.x}px, ${this.y}px)`
+        // }
     }
-    
-
     create() : void{
         this.div = document.createElement("Characters")
         this.spawnElement.appendChild(this.div);
         this.div.innerText = this.value
         this.x = 0 
     }
- 
     checkOnBelt(a: ClientRect, b: ClientRect) : boolean {
         return(a.left <= b.right &&
             b.left <= a.right &&
             a.top <= b.bottom &&
             b.top <= a.bottom)
-
             this.onBelt = true 
     }
- 
     setSpeed() : void{
         if(this.onBelt =true){
             this.speed = 2 
--- dev/game.ts
+++ dev/game.ts
@@ -1,59 +1,39 @@
-import { Hook } from "./Hook.js"
-import { Tube } from "./Tube.js"
-import { Level } from "./Level.js"
-import { Character } from "./Character.js"
- 
-class Game {
-    spawnElement : HTMLElement
-    div : HTMLElement
-    private levels : Level[] = []
-    private hook : Hook
-    private tube : Tube
-    private characters : Character[] = []
- 
-    constructor() {
-        //Level 1: using the word 'Bus'
-        this.levels = []
-        const level1: Level = new Level("B_s", "u", ["a", "u", "o", "i"])
 
-        this.selectRandomCharacter(level1)
-        this.selectRandomCharacter(level1)
-        this.selectRandomCharacter(level1)
-        this.selectRandomCharacter(level1)
-
-
-        this.spawnElement = document.querySelector('charcontainer')!;
-        this.gameloop()
-    }
-
-    //NOT WORKING
-    gameloop() {
-        for (const Characters of this.characters) {
-            Characters.update()
-            requestAnimationFrame(() => this.gameloop())
-        }
-    }
-
-  
-
-    private shuffleArray() {
-        this.characters = this.characters
-        .map((a) => ({sort: Math.random(), value: a}))
-        .sort((a, b) => a.sort - b.sort)
-        .map((a) => a.value)
-
-    }
-
-    private selectRandomCharacter(level : Level) {
-        let randomValue =  Math.floor(Math.random() * level.possibilities.length)
-        console.log(randomValue);
-        
-        if(level.possibilities.length > 0) {
-        let c = level.possibilities.splice(randomValue, 1) 
-        console.log(level.possibilities);
-        this.characters.push(new Character(c[0]))
-        }
-    }
-}
- 
-new Game()
+  import { Hook } from "./Hook.js"
+  import { Tube } from "./Tube.js"
+  import { Level } from "./Level.js"
+  import { Character } from "./Character.js"
+  class Game {
+      spawnElement : HTMLElement
+      div : HTMLElement
+      private levels : Level[] = []
+      private hook : Hook
+      private tube : Tube
+      private characters : Character[] = []
+      constructor() {
+          //Level 1: using the word 'Bus'
+          this.levels = []
+          const level1: Level = new Level("B_s", "u", ["a", "u", "o", "i", "x"])
+          //the dom element where characters are placed in
+          this.spawnElement = document.querySelector('charcontainer')!;
+          this.spawnCharacters(level1)
+          this.gameloop()
+      }
+      gameloop() {
+      }
+      spawnCharacters(level1 : Level){
+          let length : number = level1.possibilities.length
+          for (let i : number = 0; i < length; i++) {
+              this.selectRandomCharacter(level1)
+          }
+      }
+      private selectRandomCharacter(level : Level) {
+          let randomValue =  Math.floor(Math.random() * level.possibilities.length)
+          if(level.possibilities.length > 0) {
+          let c = level.possibilities.splice(randomValue, 1) 
+          console.log(level.possibilities);
+          this.characters.push(new Character(c[0]))
+          }
+      }
+  }
+  new Game()
