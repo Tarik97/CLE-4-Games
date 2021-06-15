b4a204cc6c8cb6e98d63ab9c57077a1025cfe177
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2021-06-15 20:20:34 +0200

    added "click the correct one" also "text to let you know it is the correct one"

--- dev/game.ts
+++ dev/game.ts
@@ -1,61 +1,128 @@
 
-  import { Hook } from "./Hook.js"
-  import { Tube } from "./Tube.js"
-  import { Level } from "./Level.js"
-  import { Character } from "./Character.js"
+import { Hook } from "./Hook.js"
+import { Tube } from "./Tube.js"
+import { Level } from "./Level.js"
+import { Character } from "./Character.js"
 
-  class Game {
-      spawnElement : HTMLElement
-      div : HTMLElement
-      private levels : Level[] = []
-      private hook : Hook
-      private tube : Tube
+class Game {
+    /*
+    Variables
+    */
+    spawnElement: HTMLElement
+    wordElement: HTMLElement
+    currentWordText: HTMLElement
+    icon: HTMLElement
+    private levels: Level[] = []
+    private hook: Hook
+    private tube: Tube
+    private characters: Character[] = []
+    private currentLevel: number = 0
+    /*
+    Level currently only usable
+    */
+    public level1: Level = new Level("B_s", "u", ["a", "u", "o", "i", "x"])
 
-      private characters : Character[] = []
+    /*
+    Future levels (levels shouldnt be made in the game class smh)
+    
+    public level2 : Level = new Level("T_s", "a", ["r", "a", "i", "u", "v"])
+    public level3 : Level = new Level("P_n", "e", ["h", "f", "e", "o", "a"])
+    public level4 : Level = new Level("G_m", "u", ["p", "u", "o", "i", "q"])
 
-      private currentLevel : number = 0
-      public level1 : Level = new Level("B_s", "u", ["a", "u", "o", "i", "x"])
-      public level2 : Level = new Level("T_s", "a", ["r", "a", "i", "u", "v"])
-      public level3 : Level = new Level("P_n", "e", ["h", "f", "e", "o", "a"])
-      public level4 : Level = new Level("G_m", "u", ["p", "u", "o", "i", "q"])
+    */
 
-      public constructor() {
-          
-        if (this.currentLevel == 0){
-          this.spawnCharacters(this.level1)
-        }
+    public constructor() {
+        //where the current word and icon gets appended to
+        this.wordElement = document.querySelector("word")!
+        //the element wich has the text in it
+        this.currentWordText = document.createElement("currentWordText")
+        //inner text will be that of the current level's (level1) word
+        this.currentWordText.innerText = this.level1.word
+        //appending....
+        this.wordElement.appendChild(this.currentWordText);
 
-        this.spawnElement = document.querySelector('charcontainer')!
+        //the icon element
+        this.icon = document.createElement("icon")
+        //appending....
+        this.wordElement.appendChild(this.icon);
 
+        //adding click event listener
         document.getElementById("foreground")!.addEventListener("click", this.clickHandler)
-      }
 
-      public clickHandler(e : Event){
-          let target : HTMLElement = e.target
-          console.log(target.innerText)
-      }
+        if (this.currentLevel == 0) {
+            
+            //the element where the characters get appended to
+            this.spawnElement = document.querySelector('charcontainer')!
+            //sending the level1 variable to the spawnCharacters function
+            this.spawnCharacters(this.level1)
+        }
+    }
 
-      private spawnCharacters(level1 : Level){
-          length = level1.possibilities.length;
-          for (let i : number = 0; i < length; i++) {
-              this.selectRandomCharacter(level1)
-          }
-      }
+    //function that spawns characters depending on how many character possibilities there are in the level variable
+    private spawnCharacters(level1: Level) {
+        length = level1.possibilities.length;
+        for (let i: number = 0; i < length; i++) {
+            this.selectRandomCharacter(level1)
+        }
+    }
 
-      private selectRandomCharacter(level : Level) {
+    /*
+    Click handler, checks wether the clicked character is the correct characer, 
+    if so calls displayText function to let player know.
+    This is where stuff needs to happen to load the new level, or to call the function that loades the new level
+    */
+    private clickHandler = (e: Event) => {
+        if (e.target.id == "true") {
+            console.log("Correct answer!!")
+            this.currentLevel += 1
+            this.displayText(true)
 
-          let randomValue =  Math.floor(Math.random() * level.possibilities.length)
+        }
+        else {
+            console.log("wrong answer :(")
+            this.displayText(false)
+        }
+    }
 
-          if(level.possibilities.length > 0) {
+    /*
+    displays whether the clicked character is the correct one by showing text "correct" or "incorrect" 
+    and changing the colours
+    */
+    private displayText(correct: boolean) {
+        if (correct) {
+            this.icon.classList.remove("incorrect");
+            this.icon.classList.add("correct");
+            this.icon.innerText = "correct"
 
-          let c = level.possibilities.splice(randomValue, 1) 
+        }
+        else {
+            this.icon.innerText = "incorrect"
+            this.icon.classList.remove("correct");
+            this.icon.classList.add("incorrect");
+        }
+    }
 
-          this.characters.push(new Character(c[0]))
 
-          }
-      }
+    /* 
+    selects random characters by taking a random character from the character.possibilites array, then pushing to a character instance, 
+    also parces true or false depending if its the correct character. 
+    (bad way of doing it, you can see the correct answer when pressing "inspect element" in browser)
+    */
+    private selectRandomCharacter(level: Level) {
 
+        let randomValue = Math.floor(Math.random() * level.possibilities.length)
 
-      
-  }
-  new Game()
+        if (level.possibilities.length > 0) {
+
+            let c = level.possibilities.splice(randomValue, 1)
+
+            if (c[0] == this.level1.correctAnswer) {
+                this.characters.push(new Character(c[0], true))
+            }
+            else {
+                this.characters.push(new Character(c[0], false))
+            }
+        }
+    }
+}
+new Game()
