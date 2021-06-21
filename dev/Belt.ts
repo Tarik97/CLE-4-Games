a0afe264fd638cf143adb0f951800d368e950d10
max van Schoonderwoerd <74652836+MaxvanSchoonderwoerd@users.noreply.github.com>
2021-06-21 09:57:19 +0200

    Added some variables to belt class

--- dev/Belt.ts
+++ dev/Belt.ts
@@ -1,5 +1,7 @@
 import { transform } from "../node_modules/typescript/lib/typescript"
 
 export class Belt {
-
+    private beltContainer : HTMLElement
+    private beltElem : HTMLElement
+    public beltSpeed : number
 }
